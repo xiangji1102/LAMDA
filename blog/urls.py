ba308421ad3fc0cafc965995f3a5708b335b92c4
xaingji <1498893574@qq.com>
2024-10-26 18:49:38 +0800

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path,re_path
+
+# from . import views
+
+urlpatterns = [
+    # # 个人博客首页
+    # path('blog/<str:title>/',views.blogIndexView.as_view()),
+    # # 新建文章
+    # path('article/edit',views.blogPubView.as_view()),
+    # # 博客后台
+    # path('article/backend/',views.blogBackendView.as_view()),
+    # # 编辑文章
+    # path('article/edit/<int:article_id>',views.updateArticleView.as_view()),
+    # # 删除文章
+    # path('article/delete',views.deleteArticleView.as_view()),
+    # # 文章详情页
+    # path('blog/<str:title>/article/<int:article_id>',views.articleDetailView.as_view())
+]
